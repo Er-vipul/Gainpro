1da795ec82b683597b8c4e74034c7e0ae4453013
Er-vipul <35648123+Er-vipul@users.noreply.github.com>
2024-10-05 12:46:12 +0530

    Update demo6.js

--- assets/js/demo6.js
+++ assets/js/demo6.js
@@ -1,13 +1,5 @@
 /**
  * demo2.js
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2017, Codrops
- * http://www.codrops.com
- */
 {
     // From javascript-debounce-function.
     function debounce(func, wait, immediate) {
